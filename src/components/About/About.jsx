6065017ca3bbec0316dded7694ed90a79ea22fdb
Vlad <vlad2tiger@gmail.com>
2026-03-13 12:15:05 +0300

    add aboutBeansContent

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,51 +2,60 @@ import { Component } from "react";
 import "./about.css";
 import BlackLogo from "../BlackLogo/BlackLogo";
 import products from "../../data.json";
-import Beans from "../../assets/beans_img.jpg"
+import Beans from "../../assets/beans_img.jpg";
+import Line from "../../assets/line_long_black.svg"
 
 class About extends Component {
   render() {
     return (
       <div className="about_main_container">
-          {this.props.activeTabNav === "CoffeeHouse" && (
-            <div className="about_container">
-              <p className="about_title">About Us</p>
+        {this.props.activeTabNav === "CoffeeHouse" && (
+          <div className="about_container max_width_about_house about_house_pd">
+            <p className="about_title">About Us</p>
+            <BlackLogo />
+            <p className="about_paragraph_1_mr about_content">
+              Extremity sweetness difficult behaviour he of. On disposal of as
+              landlord horrible. Afraid at highly months do things on at.
+              Situation recommend objection do intention so questions. As
+              greatly removed calling pleased improve an. Last ask him cold feel
+              met spot shy want. Children me laughing we prospect answered
+              followed. At it went is song that held help face.
+            </p>
+            <p className="about_content">
+              Now residence dashwoods she excellent you. Shade being under his
+              bed her, Much read on as draw. Blessing for ignorant exercise any
+              yourself unpacked. Pleasant horrible but confined day end
+              marriage. Eagerness furniture set preserved far recommend. Did
+              even but nor are most gave hope. Secure active living depend son
+              repair day ladies now.
+            </p>
+          </div>
+        )}
+        {this.props.activeTabNav === "OurCoffee" && (
+          <>
+          <div className="about_container max_width_about_beans flex gap_beans_goods about_beans_pd">
+            <div className="beans_img_container">
+              <img className="beans_img" src={Beans} alt="" />
+            </div>
+            <div>
+              <p className="about_title">About our beans</p>
               <BlackLogo />
-              <p className="about_paragraph_1_mr about_content">
+              <p className="about_content">
                 Extremity sweetness difficult behaviour he of. On disposal of as
-                landlord horrible. Afraid at highly months do things on at.
-                Situation recommend objection do intention so questions. As
-                greatly removed calling pleased improve an. Last ask him cold
-                feel met spot shy want. Children me laughing we prospect
-                answered followed. At it went is song that held help face.
+                landlord horrible.
               </p>
               <p className="about_content">
-                Now residence dashwoods she excellent you. Shade being under his
-                bed her, Much read on as draw. Blessing for ignorant exercise
-                any yourself unpacked. Pleasant horrible but confined day end
-                marriage. Eagerness furniture set preserved far recommend. Did
-                even but nor are most gave hope. Secure active living depend son
-                repair day ladies now.
+                Afraid at highly months do things on at. Situation recommend
+                objection do intention<br></br>so questions.<br></br>As greatly removed calling
+                pleased improve an.<br></br>Last ask him cold feel <br></br>met spot shy want.
+                Children me laughing we prospect answered followed. At it went<br></br>
+                is song that held help face.
               </p>
             </div>
-          )}
-          {this.props.activeTabNav === "OurCoffee" && (
-            <div className="about_container flex gap_beans_goods">
-              <div className="beans_img_container"><img className='beans_img' src={Beans} alt="" /></div>
-              <div>
-                <p className="about_title">About our beans</p>
-                <div>
-                  <img src="" alt="" srcset="" />
-                  <img src="" alt="" srcset="" />
-                  <img src="" alt="" srcset="" />
-                </div>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-              </div>
-            </div>
-          )}
+          </div>
+          <img src={Line} className="long_line" alt=""/>
+          </>
+        )}
       </div>
     );
   }
